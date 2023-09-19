dc46ecd66d2073dfc61da1ff7d3a3ff42a468d92
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-19 19:51:34 -0300

    feat: add scheleton application

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
+import { FiSearch } from "ract-icons/fi";
 
 function App() {
-
-
   return (
-    <h1>ola mundo</h1>
-  )
+    <div className="container">
+      <h1 className="title">buscador cep</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="digite seu cep..." />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+      <main className="main">
+        <h2>CEP: 797979797</h2>
+        <span>rua teste</span>
+        <span>rua teste 292</span>
+        <span>bairro teste</span>
+        <span>cidade teste</span>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
